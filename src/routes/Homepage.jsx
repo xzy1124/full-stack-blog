d787d74403da394d6c091e6b2bc83891ebbd44fe
ziyanbb <2932796029@qq.com>
2025-12-06 14:39:04 +0800

    面包屑导航实现

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,7 +1,62 @@
+import {Link} from 'react-router-dom'
 const HomePage = () => {
     return (
-        <div>
-            <h1>Homepage</h1>
+        <div className="mt-4 flex flex-col gap-4">
+            {/* BREADCRUMB */}
+            <div>
+                <Link to="/">
+                    Home
+                </Link>
+                <span>·</span>
+                <span className='text-blue-800'>Blogs and Articles</span>
+            </div>
+            {/* INTRODUCTION */}
+            <div className='flex items-center justify-between'>
+                {/* titles */}
+                <div>
+                    <h1 className='text-gray-800 text-2xl md:text-5xl lg:text-6xl font-bold'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h1>
+                    <p className='mt-8 text-md md:text-xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit quos impedit</p>
+                </div>
+                {/* animated button */}
+                <Link to="write" className='hidden md:block relative'>
+                {/* svg是矢量图容器 */}
+                    <svg
+                        viewBox='0 0 200 200'
+                        width="200"
+                        height="200"
+                        className='text-lg tracking-tighter animate-spin animateButton'
+                    >
+                        {/* path任意形状的路径 */}
+                        <path 
+                            id='circlePath'
+                            fill='none'
+                            d='M 100, 100 m -75, 0 a 75,75 0 1,1 150,0 a 75,75 0 1,1 -150,0'
+                        />
+                        <text>
+                            {/* textPath让文字沿着路径排列 */}
+                            <textPath href='#circlePath' startOffset='0%'>Write your story ·</textPath>
+                            <textPath href='#circlePath' startOffset='50%'>Share your idea ·</textPath>
+                        </text>
+                    </svg>
+                    <button className='absolute top-0 left-0 right-0 bottom-0 m-auto w-20 h-20 bg-blue-800 rounded-full flex items-center justify-center justify-center'>
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'    
+                            viewBox='0 0 24 24'
+                            width='50'
+                            height='50'
+                            fill='none'
+                            stroke='white'
+                            strokeWidth='2'
+                        >
+                            {/* line画线段、箭头 */}
+                            <line x1='6' y1='18' x2='18' y2='6' />
+                            <polyline points="9 6 18 6 18 15" />
+                        </svg>
+                    </button>
+                </Link>
+            </div>
+            {/* FEATURED POSTS */}
+            {/* POST LIST */}
         </div>
     )
 }
