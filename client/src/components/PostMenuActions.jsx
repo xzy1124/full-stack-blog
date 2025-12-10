7d0cb4bfb0a815b5323f0d3e3da9346dcb687b7d
ziyanbb <2932796029@qq.com>
2025-12-10 11:18:54 +0800

    PostMenuAction认证user

--- client/src/components/PostMenuActions.jsx
+++ client/src/components/PostMenuActions.jsx
@@ -1,4 +1,8 @@
-const PostMenuActions = () => {
+import {useUser, useAuth} from "@clerk/clerk-react";
+const PostMenuActions = ({post}) => {
+    // 要认证用户之后才能看到
+    const {user} = useUser()
+    const {getToken} = useAuth()
     return (
         <div className="">
             <h1 className="mt-8 mb-4 text-sm font-medium">Actions</h1>
@@ -17,28 +21,31 @@ const PostMenuActions = () => {
                 </svg>
                 <span>Save this Post</span>
             </div>
-            <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="20"
-                    height="20"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                >
-                    {/* 把手 */}
-                    <path d="M3 6h18M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-                    {/* 桶身 */}
-                    <path d="M5 6v14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6" />
-                    {/* 桶盖条纹 */}
-                    <line x1="10" y1="11" x2="10" y2="17" />
-                    <line x1="14" y1="11" x2="14" y2="17" />
-                </svg>
-                <span>Delete this Post</span>
-            </div>
+            {user && post.user.username === user.username && (
+                <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="20"
+                        height="20"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        {/* 把手 */}
+                        <path d="M3 6h18M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                        {/* 桶身 */}
+                        <path d="M5 6v14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6" />
+                        {/* 桶盖条纹 */}
+                        <line x1="10" y1="11" x2="10" y2="17" />
+                        <line x1="14" y1="11" x2="14" y2="17" />
+                    </svg>
+                    <span>Delete this Post</span>
+                </div>
+            )}
+
         </div>
     )
 }
--- client/src/routes/SinglePostPage.jsx
+++ client/src/routes/SinglePostPage.jsx
@@ -137,7 +137,7 @@ const SinglePostPage = () => {
                         </div>
                     </div>
                     {/* Actions */}
-                    <PostMenuActions />
+                    <PostMenuActions post={data.data} />
                     {/* Categories */}
                     <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
                     <div className="flex flex-col gap-2 text-sm">
