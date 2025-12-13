a4f101bc5c81ca2c98bf0a70a4dd6d2945073cd6
ziyanbb <2932796029@qq.com>
2025-12-13 10:48:30 +0800

    Clerk添加域名

--- client/src/routes/LoginPage.jsx
+++ client/src/routes/LoginPage.jsx
@@ -2,10 +2,9 @@ import { SignIn } from '@clerk/clerk-react'
 const LoginPage = () => {
     return (
         <div className='flex items-center justify-center h-[calc(100vh-80px)]'>
-            <SignIn 
-                signInUrl="/register"
-                redirectUrl="https://full-stack-blog-vncx.vercel.app"
-             />
+            <SignIn // 注册成功
+                signUpUrl="/register"                                      // 本地注册页
+            />
         </div>
     )
 }
